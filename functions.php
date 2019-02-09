80a20aacccbd6d755b8fd9cea83024ee4fbb6b4e
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-02-09 01:32:17 -0500

    Update image and styles

--- functions.php
+++ functions.php
@@ -14,12 +14,15 @@
   function my_login_logo() { ?>
     <style type="text/css">
       #login h1 a, .login h1 a {
-        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.png);
-        height:65px;
-        width:320px;
-        background-size: 320px 65px;
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/carpe-telam-logo.png);
+        height: 80px;
+        width: 80px;
+        background-size: 80px 80px;
         background-repeat: no-repeat;
         padding-bottom: 30px;
+      }
+      #backtoblog {
+        display: none;
       }
     </style>
   <?php }
