2ceae02fbf9b4234941a1ba812249305d4b245b6
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-27 01:16:30 +0000

    Remove custom login functions

--- functions.php
+++ functions.php
@@ -11,55 +11,6 @@
   }
   add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-  function my_login_logo() { ?>
-    <style type="text/css">
-      #login h1 a, .login h1 a {
-        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/carpe-telam-logo.png);
-        height: 256px;
-        width: 256px;
-        background-size: 256px 256px;
-        background-repeat: no-repeat;
-        padding-bottom: 30px;
-      }
-      #backtoblog {
-        display: none;
-      }
-    </style>
-  <?php }
-  add_action( 'login_enqueue_scripts', 'my_login_logo' );
-
-  function my_login_logo_url() {
-    return 'https://www.carpetelam.com';
-  }
-  add_filter( 'login_headerurl', 'my_login_logo_url' );
-
-  function my_login_logo_url_title() {
-    return 'Carpe Telam';
-  }
-  add_filter( 'login_headertitle', 'my_login_logo_url_title' );
-
-  /**
-   * Set the URL to redirect to on login.
-   *
-   * @return string URL to redirect to on login. Must be absolute.
-   */
-  function my_forcelogin_redirect() {
-    return home_url( '/wp-admin/' );
-  }
-  add_filter( 'v_forcelogin_redirect', 'my_forcelogin_redirect' );
-
-  /**
-   * Filter Force Login to allow exceptions for specific URLs.
-   *
-   * @param array $whitelist An array of URLs. Must be absolute.
-   * @return array
-   */
-  function my_forcelogin_whitelist( $whitelist ) {
-    $whitelist[] = home_url( '/privacy-policy/' );
-    return $whitelist;
-  }
-  add_filter( 'v_forcelogin_whitelist', 'my_forcelogin_whitelist' );
-
   /**
    * Add WPGraphQL args to custom post types
    *
